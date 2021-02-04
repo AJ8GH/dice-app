06e76eaaa36c701e4ed3f26acefe441c42a9690f
Adam Jonas <adamjonas25@gmail.com>
2021-02-04 11:33:10 +0000

    define Dice#roll, pass 2 specs

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,5 +1,11 @@
+require 'dice'
+
 describe Dice do
   describe '#roll' do
-    
+    it {is_expected.to respond_to :roll }
+
+    it 'gives a number between 1 and 6' do
+      expect(subject.roll).to be_between(1, 6)
+    end
   end
 end
