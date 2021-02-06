9da8b9cd66b165358e81cc7198a4749f022981ce
Adam Jonas <adamjonas25@gmail.com>
2021-02-06 22:49:07 +0000

    add spec for player#score

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,6 +4,9 @@ describe Player do
   it { is_expected.to respond_to :score }
 
   describe '#score' do
-
+    context 'on initialization' do
+      subject { described_class.new.score }
+      it { is_expected.to be 0 }
+    end
   end
 end
