030d444f6c99238290ddd0a6582000fd8b220241
Adam Jonas <adamjonas25@gmail.com>
2021-02-07 20:37:34 +0000

    pass test for roll

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,2 +1,11 @@
 class Dice
+  def roll(number_of_dice = 1)
+    side
+  end
+
+  private
+
+  def side
+    rand(1..6)
+  end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,3 +1,10 @@
 describe Dice do
+  it { is_expected.to respond_to(:roll).with 0..1 }
 
+  describe '#roll' do
+    context 'with no arguments' do
+      subject { described_class.new.roll }
+      it { is_expected.to be_between 1, 6}
+    end
+  end
 end
