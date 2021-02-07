a34cfb7d2f06b5584733d1789b6478f5411d8409
Adam Jonas <adamjonas25@gmail.com>
2021-02-07 20:48:21 +0000

    add sides attr reader

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,17 +1,23 @@
 class Dice
+  attr_reader :sides
+
   def roll(number_of_dice = 1)
     return_rolls(number_of_dice)
   end
 
   private
 
+  def initialize(sides = 6)
+    @sides = sides
+  end
+
   def return_rolls(number_of_dice)
     rolls = []
-    number_of_dice.times { rolls << side }
+    number_of_dice.times { rolls << result }
     rolls
   end
 
-  def side
-    rand(1..6)
+  def result
+    rand(1..sides)
   end
 end
